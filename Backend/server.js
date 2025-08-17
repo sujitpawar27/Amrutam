99b844b24554990c9ab396b648fe41f9d2c2d87b
sujitpawar <sujitpawar074@gmail.com>
2025-08-17 12:59:44 +0530

    server updated

--- Backend/server.js
+++ Backend/server.js
@@ -35,4 +35,7 @@ mongoose
 // Start server
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
